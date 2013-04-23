4ccf34a69031c3bc176508cd9c5cfcb52bb4d40c
Lee Sharma <lee@leesharma.com>
2013-04-23 18:39:50 -0400

    Added @featured to index controller

--- projectnexus/app/controllers/index_controller.rb
+++ projectnexus/app/controllers/index_controller.rb
@@ -1,6 +1,7 @@
 class IndexController < ApplicationController
   def index
     @projects = Project.all
+    @featured = Project.find_by_featured(true)
 
     respond_to do |format|
       format.html # index.html.erb                                                                                                                           
