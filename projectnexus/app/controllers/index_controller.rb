6177aa9334b6f9fe9745b9088f59df58be569f67
Lee Sharma <lee@leesharma.com>
2013-04-23 17:41:04 -0400

    Removed project limit on index; updated New Projects to sort correctly

--- projectnexus/app/controllers/index_controller.rb
+++ projectnexus/app/controllers/index_controller.rb
@@ -1,6 +1,6 @@
 class IndexController < ApplicationController
   def index
-    @projects = Project.limit(8)
+    @projects = Project.all
 
     respond_to do |format|
       format.html # index.html.erb                                                                                                                           
