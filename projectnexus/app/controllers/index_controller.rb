1627ee0e89f3ceb19a25b997741d0d9d4547c97f
Lee Sharma <lee@leesharma.com>
2013-04-23 18:47:46 -0400

    Troubleshooting
    Deleted shuffling and limitation on featured projects

--- projectnexus/app/controllers/index_controller.rb
+++ projectnexus/app/controllers/index_controller.rb
@@ -1,9 +1,7 @@
 class IndexController < ApplicationController
-  # GET /projects
-  # GET /projects.json
   def index
     @projects = Project.all
-    @featured = Project.find_by_featured(true).shuffle
+    @featured = Project.find_by_featured(true)
 
     respond_to do |format|
       format.html # index.html.erb                                                                                                                           
