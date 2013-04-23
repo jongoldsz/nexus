39e6e008ae863589307a8482af65238dff00d123
Jonathan Goldszmidt <jongoldsz@gmail.com>
2013-04-23 23:28:29 +0000

    Fixed error with limiting and shuffling featured projects

--- projectnexus/app/controllers/index_controller.rb
+++ projectnexus/app/controllers/index_controller.rb
@@ -3,11 +3,11 @@ class IndexController < ApplicationController
   # GET /projects.json
   def index
     @projects = Project.all
-    @featured = Project.find_by_featured(true)
+    @featured = Project.find(:all, :conditions => [ "featured = ?", true], :limit =>4)
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @projects }                                                                                                                           
+      format.json { render json: @projects }                                                                      
     end
   end
 end
