d9e70e12315c3b887f06336b9c9704a7539cf54b
Lee Sharma <lee@leesharma.com>
2013-04-23 17:34:47 -0400

    Changed project limit to 8

--- projectnexus/app/controllers/index_controller.rb
+++ projectnexus/app/controllers/index_controller.rb
@@ -1,6 +1,6 @@
 class IndexController < ApplicationController
   def index
-    @projects = Project.limit(4)
+    @projects = Project.limit(8)
 
     respond_to do |format|
       format.html # index.html.erb                                                                                                                           
