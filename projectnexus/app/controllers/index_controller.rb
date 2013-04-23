228627615dbb588d8ecf5c405eddad43cf9ba67c
Lee Sharma <lee@leesharma.com>
2013-04-23 18:45:29 -0400

    Troubleshooting

--- projectnexus/app/controllers/index_controller.rb
+++ projectnexus/app/controllers/index_controller.rb
@@ -1,7 +1,9 @@
 class IndexController < ApplicationController
+  # GET /projects
+  # GET /projects.json
   def index
     @projects = Project.all
-    @featured = Project.find_by_featured(true)
+    @featured = Project.find_by_featured(true).shuffle
 
     respond_to do |format|
       format.html # index.html.erb                                                                                                                           
