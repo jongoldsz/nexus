f8fa53c5da3feccc3fbeeb2072d4fd60acd5fc3a
Hieu Vu <vutrunghieu6111989@gmail.com>
2013-04-25 17:42:54 +0700

    update project item image

--- projectnexus/app/models/project.rb
+++ projectnexus/app/models/project.rb
@@ -18,9 +18,10 @@ class Project < ActiveRecord::Base
   has_many :memberships,:foreign_key => "project_id"
   has_many :blogentries,:foreign_key => "project_id"
   has_many :messages, :foreign_key => "project_id"
-  has_attached_file :photo, :style => { :tile => "200x100>" }, 
+  has_attached_file :photo, :style => { :tile => "200x100>" },
                     :url  => "/assets/images/:id/:style/:basename.:extension",
-                    :path => ":rails_root/public/assets/images/:id/:style/:basename.:extension"
+                    :path => ":rails_root/public/assets/images/:id/:style/:basename.:extension",
+                    :default_url => "/assets/nexus-logo.png"
   validates_attachment_size :photo, :less_than => 10.megabytes
 
   attr_accessible :description, :name, :pitch, :status, :user_id, :miniimage, :location_id, :category_id, :featured, :photo
