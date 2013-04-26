82af5dc6e3b8314758f3cbcee81097e7013d3d10
Jonathan Goldszmidt <jongoldsz@gmail.com>
2013-04-26 07:08:38 +0000

    Added support for using category name instead of category id to find projects by category. Also made this work with case insensitivity.

--- projectnexus/app/controllers/categories_controller.rb
+++ projectnexus/app/controllers/categories_controller.rb
@@ -13,7 +13,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
-    @category = Category.find(params[:id])
+    @category = Category.find(:first, :conditions => [ "lower(name) = ?", params[:id].downcase ])
 
     respond_to do |format|
       format.html # show.html.erb
