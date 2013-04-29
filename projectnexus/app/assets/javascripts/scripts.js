f67c60726562994c8eb7cf858e457e1fdfd1a600
Jonathan Goldszmidt <jongoldsz@gmail.com>
2013-04-29 22:40:38 +0000

    Merging.

--- projectnexus/app/assets/javascripts/scripts.js
+++ projectnexus/app/assets/javascripts/scripts.js
@@ -1,9 +1,2 @@
 $(document).ready(function(){
-  homeContact();
 });
-function homeContact(){
-  var _infoBounder = $("#contact-info-window-content");
-  _infoBounder.click(function(){
-    $(this).stop().animate({'left':"0"},'fast');
-  });
-}
--- projectnexus/config/initializers/omniauth.rb
+++ projectnexus/config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '558234490875817', 'dcd2302121e8e7cc8d48a13b5fccaefa'
+  provider :facebook, '452505471496522', '05abb6c2699b1487bd6feb3b30349011', :client_options => {:ssl => {:verify => false}}
+  #'558234490875817', 'dcd2302121e8e7cc8d48a13b5fccaefa'
 end
